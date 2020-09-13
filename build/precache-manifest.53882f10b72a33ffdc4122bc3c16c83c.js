c5c1aef020cda2ebdcebbf6ecc243822361cb2d3
Batetambe Tanyi <33326060+tanyibate@users.noreply.github.com>
2020-09-13 20:40:18 +0100

    update

---
+++ build/precache-manifest.53882f10b72a33ffdc4122bc3c16c83c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89544fae061704c40e0122baa55ed624",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "6154131922396b65d75c",
+    "url": "/tictactoe-react/static/css/main.176be72f.chunk.css"
+  },
+  {
+    "revision": "939187571492d1eaac28",
+    "url": "/tictactoe-react/static/js/2.e486b7eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe-react/static/js/2.e486b7eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6154131922396b65d75c",
+    "url": "/tictactoe-react/static/js/main.260841c9.chunk.js"
+  },
+  {
+    "revision": "514f82430784f25a752b",
+    "url": "/tictactoe-react/static/js/runtime-main.a7ab193d.js"
+  }
+]);
